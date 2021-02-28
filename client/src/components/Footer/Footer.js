80d0660957eb8abc4c0af42a09641d9ab4418772
Tom Brown <tombrown654321@protonmail.com>
2021-02-28 11:06:34 -0500

    Added scripts for server deployment

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer class="appfooter">
       &copy; Hebrew Alphabet Game - Copyright 2021
       <span className="rightfooter">
         Built by <a href="http://www.tomrembrown.com">Tom Brown</a>
